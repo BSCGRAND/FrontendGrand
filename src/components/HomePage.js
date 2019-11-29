170615153f13474f55997d3c220145a074e6ed58
Andrei <amoskvin87@gmail.com>
2019-11-29 16:56:52 +0700

    добавил инпуты для фильтров

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../axios/axios';
 import { connect} from 'react-redux';
+import './HomePage.css';
 
 
 class HomePage extends Component {
@@ -13,7 +14,7 @@ class HomePage extends Component {
                 })
         };
         const list = (this.state === null) ? <ul><li>empty</li></ul> :
-            <table border="1">
+            <table className={"requestsTable"} border={1}>
                 <tr>
                     <td>Id</td>
                     <td>Дата подачи заявки</td>
@@ -53,7 +54,39 @@ class HomePage extends Component {
             </table>;
         return (
             <div className="container__list">
-                <span>!!!HomePage!!!</span>
+                <h3>Поиск запроса на закупку</h3>
+                <div className={"filters"}>
+                <table>
+                    <tr>
+                        <td><label htmlFor={"findIdFrom"}>Id от</label></td>
+                        <td><label htmlFor={"findIdTo"}>Id до</label></td>
+                        <td><label htmlFor={"findSubdivision"}>Подразделение</label></td>
+                        <td><label htmlFor={"findDateOfRequestFrom"}>Дата подачи от</label></td>
+                        <td><label htmlFor={"findDateOfRequestTo"}>Дата подачи до</label></td>
+                        <td><label htmlFor={"findDateOfReceivingFrom"}>Дата получения от</label></td>
+                        <td><label htmlFor={"findDateOfReceivingTo"}>Дата получения до</label></td>
+                        <td><label htmlFor={"findDateOfGeneralRequestFrom"}>Дата общей заявки от</label></td>
+                        <td><label htmlFor={"findDateOfGeneralRequestTo"}>Дата общей заявки до</label></td>
+                        <td><label htmlFor={"findSupply"}>Поставка</label></td>
+                        <td><label htmlFor={"findSent"}>Отправлено</label></td>
+                        <td><label htmlFor={"findProgressMark"}>Отметка о выполнении</label></td>
+                    </tr>
+                    <tr>
+                        <td><input id={"findIdFrom"}/></td>
+                        <td><input id={"findIdTo"}/></td>
+                        <td><input id={"findSubdivision"}/></td>
+                        <td><input id={"findDateOfRequestFrom"}/></td>
+                        <td><input id={"findDateOfRequestTo"}/></td>
+                        <td><input id={"findDateOfReceivingFrom"}/></td>
+                        <td><input id={"findDateOfReceivingTo"}/></td>
+                        <td><input id={"findDateOfGeneralRequestFrom"}/></td>
+                        <td><input id={"findDateOfGeneralRequestTo"}/></td>
+                        <td><input id={"findSupply"}/></td>
+                        <td><input id={"findSent"}/></td>
+                        <td><input id={"findProgressMark"}/></td>
+                    </tr>
+                </table>
+            </div>
                 <button type="submit" onClick={loadGoodsRequests}>Find</button>
                 <goodsRequestList>
                     {list}
